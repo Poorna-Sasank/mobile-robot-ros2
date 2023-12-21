1d45a24e52a3be7fb0a63c7693c50043b20c0ce0
POORNA SASANK SESETTI <62737084+Poorna-Sasank@users.noreply.github.com>
2023-12-21 14:09:23 +0530

    Delete build directory

--- build/kshoonya_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/comrade/kshoonya_ws/src/kshoonya_description
-# Build directory: /home/comrade/kshoonya_ws/build/kshoonya_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/my_robot_packages/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/comrade/kshoonya_ws/install/my_robot_packages'
